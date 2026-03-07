203a7987b26044e7156e542a507e29d819e49d8e
SolarBlackhole <dylanwyckoff@gmail.com>
2026-03-06 21:14:51 -0500

    Fixes

--- cogs/tickets.py
+++ cogs/tickets.py
@@ -1,7 +1,7 @@
 import discord
+from discord.ext import commands
 from discord import app_commands
 from helpers.tickets_helper import TicketsHelper
-from dotenv import load_dotenv
 import asyncio
 import os
 
